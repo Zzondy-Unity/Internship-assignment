341fbcae69f723d34efae6e84c2105c748e28730
Zzondy-Unity <abdos230@naver.com>
2025-02-16 00:45:40 +0900

    Fix : 화면밖에서 맞을경우 데미지 입지 않게 변경

--- Assets/02_Scripts/Player/ProjectileController.cs
+++ Assets/02_Scripts/Player/ProjectileController.cs
@@ -4,17 +4,19 @@ public class ProjectileController : MonoBehaviour
 {
     // 화살 풀링 생각
     private Arrow _arrow;
+    private Camera _camera;
     [SerializeField] private Transform arrowPivot;
 
     public void Init()
     {
         _arrow = Managers.Resource.LoadAsset<Arrow>(Constants.arrowPrefabPath + "/normalArrow");
+        _camera = Camera.main;
     }
 
     public void Fire(Vector3 direction, int damage)
     {
         var arrow = Instantiate(_arrow);
         arrow.transform.position = arrowPivot.position;
-        arrow.Init(direction, damage);
+        arrow.Init(direction, damage, _camera);
     }
 }
--- Assets/02_Scripts/Projectile/Arrow.cs
+++ Assets/02_Scripts/Projectile/Arrow.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class Arrow : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Camera _mainCamera;
     private int _damage;
     private int _speed = 10;
     
-    public void Init(Vector3 direction, int damage)
+    public void Init(Vector3 direction, int damage, Camera mainCamera)
     {
         rb = GetComponent<Rigidbody2D>();
         _damage = damage;
+        _mainCamera = mainCamera;
         
         rb.linearVelocity = direction * _speed;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Enemy") && IsVisibleOnScreen(other.transform.position))
         {
             if (other.gameObject.TryGetComponent(out Monster monster) && monster.isAlive)
             {
@@ -31,12 +33,20 @@ public class Arrow : MonoBehaviour
             }
         }
     }
-
+    
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Wall"))
         {
             Destroy(gameObject);
         }
+    }
+    
+    private bool IsVisibleOnScreen(Vector3 transformPosition)
+    {
+        if (_mainCamera == null) return false;
+        
+        Vector3 viewPoint = _mainCamera.WorldToViewportPoint(transformPosition);
+        return viewPoint.x >= 0 && viewPoint.x <= 1 && viewPoint.y >= 0 && viewPoint.y <= 1;
     }
 }
